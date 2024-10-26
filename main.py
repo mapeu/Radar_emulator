bcb834ecb645d81edca818c82bf288d9b4e24efe
Dmitry <newmapeu1@gmail.com>
2024-10-26 20:26:54 +0300

    Вторая версия

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ background_num = 1000
 # Установим параметры радара
 range_max = 100
 
+# Класс для хранения координат и скорости точки
+class Point:
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+
 # Создаем фигуру и оси
 fig, ax = plt.subplots()
 ax.set_xlim(-range_max, range_max)
@@ -18,76 +26,68 @@ ax.set_aspect('equal')
 ax.set_title('Симуляция радара')
 
 # Генерация движущихся целей
-target_x = np.random.uniform(-range_max, range_max, num_targets)
-target_y = np.random.uniform(-range_max, range_max, num_targets)
-target_vx = np.random.uniform(-5, 5, num_targets)
-target_vy = np.random.uniform(-5, 5, num_targets)
+targets = [Point(np.random.uniform(-range_max, range_max),
+                 np.random.uniform(-range_max, range_max),
+                 np.random.uniform(-5, 5),
+                 np.random.uniform(-5, 5)) for _ in range(num_targets)]
 
 # Отображение движущихся целей
-target_plot, = ax.plot(target_x, target_y, 'ro', markersize=5)
+target_plot, = ax.plot([target.x for target in targets], [target.y for target in targets], 'ro', markersize=5)
 
 # Функция для генерации динамического шума вокруг целей
-def generate_noise(target_x, target_y, num_noise):
+def generate_noise(targets, num_noise):
     noise_x = []
     noise_y = []
-    for x, y in zip(target_x, target_y):
+    for target in targets:
         # Генерация шума вокруг каждой цели с использованием нормального распределения
         num = np.random.randint(1, num_noise)
-        noise_x.extend(np.random.normal(loc=x, scale=5, size=num // len(target_x)))
-        noise_y.extend(np.random.normal(loc=y, scale=5, size=num // len(target_y)))
+        noise_x.extend(np.random.normal(loc=target.x, scale=5, size=num // len(targets)))
+        noise_y.extend(np.random.normal(loc=target.y, scale=5, size=num // len(targets)))
     return np.array(noise_x), np.array(noise_y)
 
 # Начальная генерация шума
-noise_x, noise_y = generate_noise(target_x, target_y, num_noise)
+noise_x, noise_y = generate_noise(targets, num_noise)
 
 # Отображение шумовых точек с прозрачностью
 noise_plot, = ax.plot(noise_x, noise_y, 'ro', markersize=2, alpha=0.3)
 
-# Параметры фона шума
-background_x = np.random.uniform(-range_max, range_max, background_num)  # X координаты для фонового шума
-background_y = np.random.uniform(-range_max, range_max, background_num)  # Y координаты для фонового шума
+# Функция для генерации фонового шума
+def generate_background_noise(num):
+    return np.random.uniform(-range_max, range_max, num), np.random.uniform(-range_max, range_max, num)
+
+# Начальная генерация фонового шума
+background_x, background_y = generate_background_noise(background_num)
 background_plot, = ax.plot(background_x, background_y, 'k.', markersize=1, alpha=0.2)
 
 # Функция анимации
 def animate(i):
-    global target_x, target_y, target_vx, target_vy, noise_x, noise_y, background_x, background_y
+    global targets, noise_x, noise_y, background_x, background_y
 
     # Обновление движущихся целей
-    target_x += target_vx
-    target_y += target_vy
+    for target in targets:
+        target.x += target.vx
+        target.y += target.vy
 
-    # Проверка на выход за границы
-    out_of_bounds = (np.abs(target_x) > range_max) | (np.abs(target_y) > range_max)
-
-    # Удаление целей, вышедших за границы
-    target_x = target_x[~out_of_bounds]
-    target_y = target_y[~out_of_bounds]
-    target_vx = target_vx[~out_of_bounds]
-    target_vy = target_vy[~out_of_bounds]
+    # Проверка на выход за границы и удаление целей, вышедших за границы
+    targets = [target for target in targets if np.abs(target.x) <= range_max and np.abs(target.y) <= range_max]
 
     # Генерация новых целей, если это необходимо
-    if len(target_x) < num_targets:
-        new_targets = num_targets - len(target_x)
-        new_x = np.random.uniform(-range_max, range_max, new_targets)
-        new_y = np.random.uniform(-range_max, range_max, new_targets)
-        new_vx = np.random.uniform(-5, 5, new_targets)
-        new_vy = np.random.uniform(-5, 5, new_targets)
-
-        target_x = np.concatenate((target_x, new_x))
-        target_y = np.concatenate((target_y, new_y))
-        target_vx = np.concatenate((target_vx, new_vx))
-        target_vy = np.concatenate((target_vy, new_vy))
+    while len(targets) < num_targets:
+        new_target = Point(np.random.uniform(-range_max, range_max),
+                           np.random.uniform(-range_max, range_max),
+                           np.random.uniform(-5, 5),
+                           np.random.uniform(-5, 5))
+        targets.append(new_target)
 
     # Обновление шумовых точек вокруг движущихся целей
-    noise_x, noise_y = generate_noise(target_x, target_y, num_noise)
+    noise_x, noise_y = generate_noise(targets, num_noise)
 
     # Обновление графиков
-    target_plot.set_data(target_x, target_y)
+    target_plot.set_data([target.x for target in targets], [target.y for target in targets])
     noise_plot.set_data(noise_x, noise_y)
 
-    # Обновление фонового шума
-    background_x += np.random.choice([1, -1]) * 0.1
-    background_y += np.random.choice([1, -1]) * 0.1
+    # Генерация нового фонового шума
+    background_x, background_y = generate_background_noise(background_num)
     background_plot.set_data(background_x, background_y)
 
     return target_plot, noise_plot, background_plot
